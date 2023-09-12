0cc3d1d61fbf6c05083c8d328ba58b828f731d00
amrhus <amr.hussien@camelan.com>
2023-09-12 13:16:45 +0300

    init

---
+++ src/main.ssr.js
@@ -0,0 +1,13 @@
+import App from './App.vue'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+export const AppInstance = App;
